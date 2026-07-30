135e065f3aa7ccf3c28c1ca112a89afa099ffb35
Steven Gutstein <s.m.gutstein@gmail.com>
2026-07-30 13:39:39 -0400

    Minor edits

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='Multimodal RAG Shopping Chatbot',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/ravasarala/ShopTalk',
+    url='https://github.com/smgutstein/ShopTalk',
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
